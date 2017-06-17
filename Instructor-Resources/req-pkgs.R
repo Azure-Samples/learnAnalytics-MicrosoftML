c04437a1e52ce3c45c4034051ac6d6311b1af2a0
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-06-17 12:08:10 +0000

    Add makefile
    Adds a makefile and required r packages to instructor resources directory

---
+++ Instructor-Resources/req-pkgs.R
@@ -0,0 +1,23 @@
+## set CRAN to latest
+r <- getOption('repos')
+mran_date <- Sys.Date() - 1
+r[["CRAN"]] <- paste0("https://mran.revolutionanalytics.com/snapshot/", mran_date)
+options(repos = r)
+
+## install CRAN packages
+reg_pkgs <- c("dplyr", "purrr", "tidyr", 
+              "ggplot2", "scales", "magrittr", 
+              "foreach", "readr", 
+              "maps", "scales", "foreach", "devtools")
+pks_missing <- reg_pkgs[!(reg_pkgs %in% installed.packages()[, 1])]
+install.packages(c(pks_missing, 'knitr', 'formatR', 'rmarkdown'))
+
+## install github packages
+dev_pkgs <- c("RevolutionAnalytics/dplyrXdf", "jbkunst/d3wordcloud")
+dev_names <- c("dplyrXdf", "d3wordcloud")
+dev_pkgs <- dev_pkgs[!(dev_names %in% installed.packages()[, 1])]
+
+devtools::install_github(dev_pkgs)
+
+pkgs <- c(reg_pkgs, dev_names)
+Map(packageVersion, pkgs)
