a77c36334d17329627e8e1889baeb3de39388d23
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-11-01 09:03:01 +0000

    update download url

--- Instructor-Resources/install-rstudio-ubuntu.sh
+++ Instructor-Resources/install-rstudio-ubuntu.sh
@@ -8,11 +8,11 @@ RSTUDIO_VERSION=$ARG1
 # RSTUDIO_FILE=rstudio-server-rhel-"$RSTUDIO_VERSION"-x86_64.rpm
 RSTUDIO_FILE=rstudio-server-"$RSTUDIO_VERSION"-amd64.deb
 
-wget https://s3.amazonaws.com/rstudio-dailybuilds/"$RSTUDIO_FILE"
+wget https://s3.amazonaws.com/rstudio-ide-build/server/precise/amd64/"$RSTUDIO_FILE"
+# wget https://s3.amazonaws.com/rstudio-dailybuilds/"$RSTUDIO_FILE"
 
 apt-get install -y gdebi-core 
 gdebi --option=APT::Get::force-yes=1,APT::Get::Assume-Yes=1 -n "$RSTUDIO_FILE"
-
 
 ## remove old files
 rm $RSTUDIO_FILE
